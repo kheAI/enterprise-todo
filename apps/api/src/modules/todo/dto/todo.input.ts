a8d95437bcaf7c50da49492abc295a79112d3bf9
Kai Chew <kafechew@gmail.com>
2026-06-15 23:57:02 +0800

    Input DTOs

--- apps/api/src/modules/todo/dto/todo.input.ts
+++ apps/api/src/modules/todo/dto/todo.input.ts
@@ -1,13 +1,28 @@
-import { Field, InputType } from '@nestjs/graphql';
-import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+// apps/api/src/modules/todo/dto/todo.input.ts
+import { Field, InputType, Int } from '@nestjs/graphql';
+import {
+  IsBoolean,
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 import { TodoStatus } from '../todo.constant';
 
 @InputType()
 export class CreateTodoInput {
   @Field()
-  @IsNotEmpty()
   @IsString()
+  @IsNotEmpty({ message: 'Todo text cannot be empty' })
+  @MaxLength(500, { message: 'Todo text cannot exceed 500 characters' })
   text: string;
+
+  // Part 07: userId removed from @Field and injected from JWT instead
+  @Field(() => Int)
+  @IsInt()
+  userId: number;
 }
 
 @InputType()
@@ -15,6 +30,8 @@ export class UpdateTodoInput {
   @Field({ nullable: true })
   @IsOptional()
   @IsString()
+  @IsNotEmpty()
+  @MaxLength(500)
   text?: string;
 
   @Field({ nullable: true })
@@ -24,5 +41,6 @@ export class UpdateTodoInput {
 
   @Field(() => TodoStatus, { nullable: true })
   @IsOptional()
+  @IsEnum(TodoStatus)
   status?: TodoStatus;
 }
