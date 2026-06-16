8f3a9c9664db325a08ede9523390590a2cdbbd37
Kai Chew <kafechew@gmail.com>
2026-06-16 10:38:34 +0800

    Main Page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,14 @@
+// apps/web/src/app/page.tsx
+import { TodoList } from '../components/todo-list';
+
+export default function HomePage() {
+  return (
+    <main className="min-h-screen bg-background p-8">
+      <TodoList />
+    </main>
+  );
+}
+
 export default function Index() {
   /*
    * Replace the elements below with your own.
